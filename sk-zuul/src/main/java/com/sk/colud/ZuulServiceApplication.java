f14b188fd36a78e699e14956779a033099a56588
xuhengx <xuhengxing@hongbo.net.cn>
2019-06-03 15:20:29 +0800

    12

---
+++ sk-zuul/src/main/java/com/sk/colud/ZuulServiceApplication.java
@@ -0,0 +1,31 @@
+package com.sk.colud;
+/**
+* 2019年5月31日 下午1:47:55
+* @HXing xu
+* sk-zuul
+* 
+**/
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import cn.hutool.core.util.NetUtil;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ZuulServiceApplication {
+   public static void main(String[] args) {
+       int port = 8040;
+       if(!NetUtil.isUsableLocalPort(port)) {
+           System.err.printf("端口%d被占用了，无法启动%n", port );
+           System.exit(1);
+       }
+       new SpringApplicationBuilder(ZuulServiceApplication.class).properties("server.port=" + port).run(args);
+
+   }
+}
